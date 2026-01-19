6766610f29b4253ccaa1b3a582c00ec6e5afbb41
siwoo <siwoo0312@khu.ac.kr>
2026-01-20 00:04:03 +0900

    feat: 모델 작성

---
+++ backend/book/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    translator = models.CharField(max_length=100, blank=True)
+    publisher = models.CharField(max_length=100)
+    tags = models.CharField(max_length=200)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13)
+    pages = models.IntegerField()
+    cover_image = models.URLField()
+    description = models.TextField()
+    rating = models.FloatField()
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100)
+    interest_book = models.ManyToManyField(Book, related_name="interested_users")
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    books = models.ManyToManyField(Book, related_name="authors")
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    posted_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField()
+    level = models.IntegerField()
+    comment = GenericRelation("Comment", related_query_name="review")
+
+
+class Sector(models.Model):
+    name = models.CharField(max_length=200)
+    book = models.ManyToManyField(Book, related_name="Sector")
+
+
+class Comment(models.Model):
+    # 1. 작성자 및 내용
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # 2. 어떤 모델에 대한 댓글인지 설정 (Generic Relation)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    # 3. 대댓글 구현 (자기 참조)
+    # parent가 null이면 원본 댓글, 값이 있으면 그 댓글의 대댓글입니다.
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="replies"
+    )
+
+
+class BookRequest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book_title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    translator = models.CharField(max_length=100, blank=True)
+    publisher = models.CharField(max_length=100)
+    reason = models.TextField()
+    requested_at = models.DateTimeField(auto_now_add=True)
+    comment = GenericRelation("Comment", related_query_name="book_request")
