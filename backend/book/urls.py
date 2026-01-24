8b54307fb91d2516cc217881f980c8223e60b017
siwoo <siwoo0312@khu.ac.kr>
2026-01-24 22:45:24 +0900

    Revert "feat: 북 리뷰 등록 api"
    This reverts commit 61add94a9d769f29d066b40ee5b3e71443d5fc55.

--- backend/book/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from rest_framework import routers
-from .views import ReviewViewSet
-
-router = routers.DefaultRouter()
-router.register(r"posts", ReviewViewSet)
-urlpatterns = router.urls
